c6a86eea0542f5f86e8f6f7981f21b49173278d4
Wael <waelassaf121@gmail.com>
2023-01-17 21:09:50 +0300

    Initial Push

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div>
+            <h1 className="text-6xl mb-4">About Us</h1>
+            <p>Github Finder App</p>
+        </div>
+    )
+}
+
+export default About
