4497ba7224042117fcdd95ced664e9856ba23008
Wael <waelassaf121@gmail.com>
2023-01-18 01:38:37 +0300

    Improve Styling

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import Spinner from "../layout/Spinner"
+import UserItem from "./UserItem"
 
 const UserResults = () => {
     const [users, setUsers] = useState([])
@@ -26,7 +27,7 @@ const UserResults = () => {
         md:grid-cols-2"
         >
             {users.map((user) => (
-                <h3>{user.login}</h3>
+                <UserItem key={user.id} user={user} />
             ))}
         </div>
     )
