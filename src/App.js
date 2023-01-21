f985f5422374c09c3568d8346bf55ea7ca8e7399
Wael <waelassaf121@gmail.com>
2023-01-22 02:24:12 +0300

    Add User Route

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Footer from "./components/layout/Footer"
 import Alert from "./components/layout/Alert"
 import { GithubProvider } from "./context/github/GithubContext"
 import { AlertProvider } from "./context/alert/AlertContext"
+import User from "./pages/User"
 
 const App = () => {
     return (
@@ -19,6 +20,7 @@ const App = () => {
                             <Alert />
                             <Routes>
                                 <Route path="/" element={<Home />} />
+                                <Route path="/user/:login" element={<User />} />
                                 <Route path="/about" element={<About />} />
                                 <Route
                                     path="/notfound"
